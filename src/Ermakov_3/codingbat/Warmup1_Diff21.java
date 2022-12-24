37c70ddc349c779e07eff0c9003ba4b12bc00f4f
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2022-12-24 22:03:14 +0300

    Commit

--- src/Ermakov_3/codingbat/Warmup1_Diff21.java
+++ src/Ermakov_3/codingbat/Warmup1_Diff21.java
@@ -2,7 +2,6 @@ package Ermakov_3.codingbat;
 
 public class Warmup1_Diff21 {
     public static int diff21(int n) {
-        if (n < 21) return 21 - n;
-        return Math.abs((21 - n) * 2);
+        return n < 21 ? 21 - n : Math.abs((21 - n) * 2);
     }
 }
