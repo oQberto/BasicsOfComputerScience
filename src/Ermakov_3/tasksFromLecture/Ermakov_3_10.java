a4d2ff352270978d90070325848202ada838901e
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2022-12-28 19:46:09 +0300

    Delete variable "answer"

--- src/Ermakov_3/tasksFromLecture/Ermakov_3_10.java
+++ src/Ermakov_3/tasksFromLecture/Ermakov_3_10.java
@@ -2,7 +2,6 @@ package Ermakov_3.tasksFromLecture;
 
 public class Ermakov_3_10 {
     public static String cubicEquation(int A, int B, int C, int D) {
-        String answer = "";
         double a = (double) B / A;
         double b = (double) C / A;
         double c = (double) D / A;
@@ -15,19 +14,18 @@ public class Ermakov_3_10 {
         if (discriminant > 0) {
             double X1 = (Math.round(Math.pow((((q * (-1)) / 2) + Math.sqrt(discriminant)), 1.0 / 3.0))) +
                         (Math.round(Math.pow((((q * (-1)) / 2) - Math.sqrt(discriminant)), 1.0 / 3.0))) - (a / 3);
-            answer = "Equation has got one root: X = " + X1;
+            return "Equation has got one root: X = " + X1;
         } else if ((int) discriminant == 0) {
             double X1 = -2 * (Math.pow((q / 2), 1.0 / 3.0)) - (a / 3);
             double X23 = (Math.pow((q / 2), 1.0 / 3.0)) - (a / 3);
-            answer = "Equation has got three roots: X1 = " + X1 + ", X2 = X3 = " + X23;
+            return "Equation has got three roots: X1 = " + X1 + ", X2 = X3 = " + X23;
         } else {
             double temp = (1.0 / 3.0) *
                     Math.pow(Math.sin(((3 * Math.sqrt(3) * q) / (2 * Math.pow(Math.sqrt(-p), 3)))), -1);
             double X1 = (2 / Math.sqrt(3)) * Math.sqrt(-p) * Math.sin(temp) - (a / 3);
             double X2 = (2 / Math.sqrt(3)) * Math.sqrt(-p) * Math.sin(temp + (Math.PI / 3)) - a / 3;
             double X3 = (2 / Math.sqrt(3)) * Math.sqrt(-p) * Math.cos(temp + (Math.PI / 6)) - a / 3;
-            answer = "Equation has got three roots: X1 = " + X1 + ", X2 = " + X2 + ", X3 = " + X3;
+            return "Equation has got three roots: X1 = " + X1 + ", X2 = " + X2 + ", X3 = " + X3;
         }
-        return answer;
     }
 }
---
+++ src/Ermakov_4/Ermakov_4_9.java
@@ -0,0 +1,8 @@
+package Ermakov_4;
+
+public class Ermakov_4_9 {
+    public static String vampiresNumber() {
+
+        return "";
+    }
+}
