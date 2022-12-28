12587823acee64c58658218b96df99cf07280955
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2022-12-28 20:51:28 +0300

    Add 4.9 task Add test of 4.9 task in Main class

--- src/Ermakov_4/Ermakov_4_9.java
+++ src/Ermakov_4/Ermakov_4_9.java
@@ -1,8 +1,38 @@
 package Ermakov_4;
 
-public class Ermakov_4_9 {
-    public static String vampiresNumber() {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-        return "";
+public class Ermakov_4_9 {
+    static String[] array1 = null;
+    static String[] array2 = null;
+    public static ArrayList<String> vampiresNumber() {
+        ArrayList<String> methodOutPut = new ArrayList<>();
+        for (int i = 10; i < 100; i++) {
+            for (int j = i + 1; j < 100; j++) {
+                int desiredNum = i * j;
+                if (desiredNum < 1000) {
+                    continue;
+                }
+
+                array1 = makeArray(desiredNum);
+                array2 = makeArray(i, j);
+                Arrays.sort(array1);
+                Arrays.sort(array2);
+
+                if (Arrays.equals(array1, array2)) {
+                    methodOutPut.add(String.valueOf(desiredNum));
+                }
+            }
+        }
+        return methodOutPut;
+    }
+
+    private static String[] makeArray(int arrayContent) {
+        return String.valueOf(arrayContent).split("");
+    }
+
+    private static String[] makeArray(int factor1, int factor2) {
+        return (String.valueOf(factor1) + factor2).split("");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import Ermakov_3.tasksFromLecture.Ermakov_3_10;
-import Ermakov_3.tasksFromLecture.Ermakov_3_7;
-import Ermakov_3.tasksFromLecture.Ermakov_3_8;
+import Ermakov_4.Ermakov_4_9;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Ermakov_3_8.equation());
+        System.out.println(Ermakov_4_9.vampiresNumber());
     }
 }
