2b02838efb8ef3977a742cc2021aa3ad6934f258
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2022-12-31 11:57:45 +0300

    Added task 5.6

---
+++ src/Ermakov_5/Ermakov_5_6.java
@@ -0,0 +1,17 @@
+package Ermakov_5;
+
+public class Ermakov_5_6 {
+    public static int[] sortFromMaxToMin(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                int temp;
+                if (array[j + 1] > array[j]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
