6d8d003fe717983ca4736e6f4279542167346891
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2022-12-31 11:14:09 +0300

    Added task 5.4

---
+++ src/Ermakov_5/Ermakov_5_4.java
@@ -0,0 +1,11 @@
+package Ermakov_5;
+
+public class Ermakov_5_4 {
+    public static int arrayElementsSum(int[] array) {
+        int arrayElementsSum = 0;
+        for (int element : array) {
+            arrayElementsSum += element;
+        }
+        return arrayElementsSum;
+    }
+}
