7e60d781f2b04516ae9b1f264b41f2ffc7de257e
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-01 17:33:34 +0300

    Added task 5.11.6

---
+++ src/Ermakov_5/Ermakov_5_11/Ermakov_5_11_6.java
@@ -0,0 +1,33 @@
+package Ermakov_5.Ermakov_5_11;
+
+import Ermakov_5.Ermakov_5_6;
+
+public class Ermakov_5_11_6 {
+    public static int[][] sortFromMaxToMin(int[][] array) {
+        return convert1dTo2d(array, convert2dTo1d(array));
+    }
+
+    private static int[] convert2dTo1d(int[][] array) {
+        int[] intermediateArray = new int[20];
+        int temp = 0;
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                intermediateArray[temp] = anInt;
+                temp++;
+            }
+        }
+        Ermakov_5_6.sortFromMaxToMin(intermediateArray);
+        return intermediateArray;
+    }
+
+    private static int[][] convert1dTo2d(int[][] array, int[] intermediateArray) {
+        int temp1 = -1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                temp1++;
+                array[i][j] = intermediateArray[temp1];
+            }
+        }
+        return array;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import Ermakov_5.Ermakov_5_1;
-import Ermakov_5.Ermakov_5_3;
+import Ermakov_5.Ermakov_5_10;
+import Ermakov_5.Ermakov_5_11.Ermakov_5_11_6;
 
 import java.util.Arrays;
 
 public class Main {
     private final static Ermakov_5_1 ermakov_5_1 = new Ermakov_5_1();
-    private final static int[] testArray = ermakov_5_1.makeArray();
+    private final static Ermakov_5_10 ermakov_5_10 = new Ermakov_5_10();
+    private final static int[] testArray = ermakov_5_1.doArray();
+    private final static int[][] testArray2 = ermakov_5_10.doTwoDimensionalArray();
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(testArray));
-        System.out.println(Ermakov_5_3.printEvenElements(testArray));
+        printTwoDimensionalArray(testArray2);
+        System.out.println();
+        printTwoDimensionalArray(Ermakov_5_11_6.sortFromMaxToMin(testArray2));
+    }
+
+    private static void printTwoDimensionalArray(int[][] array) {
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
     }
 }
