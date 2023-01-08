3fd0d34c72b1c985e3d9a6f2f61e302193ed57ab
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-08 15:57:33 +0300

    Changed "doTwoDimensionalArray" method Added the possibility to create a two-dimensional array of any dimension.

--- src/Ermakov_5/Ermakov_5_10.java
+++ src/Ermakov_5/Ermakov_5_10.java
@@ -1,8 +1,8 @@
 package Ermakov_5;
 
 public class Ermakov_5_10 {
-    public int[][] doTwoDimensionalArray() {
-        int[][] defaultArray = new int[4][5];
+    public int[][] doTwoDimensionalArray(int length, int width) {
+        int[][] defaultArray = new int[length][width];
         for (int i = 0; i < defaultArray.length; i++) {
             for (int j = 0; j < defaultArray[i].length; j++) {
                 defaultArray[i][j] = (int) (Math.random() * 100);
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import Ermakov_5.Ermakov_5_1;
 import Ermakov_5.Ermakov_5_10;
-import Ermakov_6.Ermakov_6_4;
+import Ermakov_6.Ermakov_6_3;
 
 import java.util.Arrays;
 
@@ -8,10 +8,11 @@ public class Main {
     private final static Ermakov_5_1 ermakov_5_1 = new Ermakov_5_1();
     private final static Ermakov_5_10 ermakov_5_10 = new Ermakov_5_10();
     private final static int[] testArray = ermakov_5_1.doArray();
-    private final static int[][] testArray2 = ermakov_5_10.doTwoDimensionalArray();
+    private final static int[][] testArray2 = ermakov_5_10.doTwoDimensionalArray(4, 5);
     public static void main(String[] args) {
         System.out.println(Arrays.toString(testArray));
-        System.out.println(Arrays.toString(Ermakov_6_4.mergeSort(testArray)));
+        Ermakov_6_3.bubbleSort(testArray);
+        System.out.println(Arrays.toString(testArray));
     }
 
     private static void printTwoDimensionalArray(int[][] array) {
