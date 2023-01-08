adf2b0cb146ef9257bcdad7b0bb95e3ea6fab7ff
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-08 16:36:04 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(testArray));
         Ermakov_6_3.bubbleSort(testArray);
+        System.out.println();
         System.out.println(Arrays.toString(testArray));
     }
 
